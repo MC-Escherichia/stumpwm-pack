8f8fee95e41852486d077b5969406de077337738
Matthew Conway <matthew.f.conway@gmail.com>
2016-01-07 13:44:25 -0500

    fix lazy loading swank

--- swank-hack.lisp
+++ swank-hack.lisp
@@ -2,33 +2,34 @@
 (in-package :stumpwm)
 ;;
 
-;; (ql:quickload "clx")
-;; (ql:quickload "cl-ppcre")
+(ql:quickload :clx)
+(ql:quickload :cl-ppcre)
 (ql:quickload :swank)
-
 
 
 (let ((server-running nil)
       (first-time t))
-  ;;
   (defcommand swank () ()
     "Toggle the swank server on/off."
     (if server-running
         (progn
-           (swank:stop-server 4005)
+           (swank:stop-server 4004)
           (echo-string
            (current-screen)
            "Stopping swank.")
           (setf server-running nil))
         (progn
-           (swank:create-server :port 4005
-                                :style :spawn
-                                :dont-close t)
+          (swank:create-server :port 4004
+                               :style :spawn
+                               :dont-close t)
 	  (if first-time
-            (echo-string
-             (current-screen)
-             "Re-starting swank.")
-	    (setf first-time nil))
+              (progn  (message "%s"
+                               (current-screen)
+                               "Starting Swank.")
+                      (setf first-time nil))
+              (echo-string
+               (current-screen)
+               "Re-starting swank."))
           (setf server-running t)))))
 
 (swank)
